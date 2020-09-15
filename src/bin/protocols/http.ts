98985bba0012a3bca50fc070afb85babd1bf5173
Gustavo <imartytk@hotmail.com>
2020-09-15 17:54:54 -0300

    controller and http types

---
+++ src/bin/protocols/http.ts
@@ -0,0 +1,14 @@
+export type HttpResponse = {
+    statusCode: number;
+    body: any;
+  };
+  
+  export type HttpRequest = {
+    body?: any;
+    headers?: any;
+    io?: any;
+    params?: any;
+    userId?: string;
+    connectedUsers?: any;
+  };
+  
