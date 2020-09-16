48dfc3db59f8bed7213c55582fe6d2099456caf5
Gustavo <imartytk@hotmail.com>
2020-09-16 13:06:44 -0300

    login controller and router

--- src/modules/user/routes/user-router.ts
+++ src/modules/user/routes/user-router.ts
@@ -1,8 +1,10 @@
 import { adaptRoute } from '@/bin/configuration/adapters/express-route-adapter';
 import { Router } from 'express';
+import { makeLoginController } from '../factories/controllers/login-factory-controller';
 import { makeSignUpController } from '../factories/controllers/signup-factory-controller';
 
 const router = Router();
 router.post('/register', adaptRoute(makeSignUpController()));
+router.post('/authenticate', adaptRoute(makeLoginController()));
 
 export default router;
