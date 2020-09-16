2dac71a949b5e074a19628e64d1149e58e8b7203
Gustavo <imartytk@hotmail.com>
2020-09-16 16:34:31 -0300

    complete register route and factory of controller

--- src/modules/user/routes/user-router.ts
+++ src/modules/user/routes/user-router.ts
@@ -2,9 +2,14 @@ import { adaptRoute } from '@/bin/configuration/adapters/express-route-adapter';
 import { Router } from 'express';
 import { makeLoginController } from '../factories/controllers/login-factory-controller';
 import { makeSignUpController } from '../factories/controllers/signup-factory-controller';
+import { auth } from '@/bin/middlewares/auth';
 
 const router = Router();
 router.post('/register', adaptRoute(makeSignUpController()));
 router.post('/authenticate', adaptRoute(makeLoginController()));
-
+router.put(
+  '/completeRegister',
+  auth,
+  adaptRoute(makeCompleteRegisterController()),
+);
 export default router;
