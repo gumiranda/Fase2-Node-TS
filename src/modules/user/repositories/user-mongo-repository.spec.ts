8aedbdbb4de1ad2e62fb6023293e487395e99c24
Gustavo <imartytk@hotmail.com>
2020-09-16 14:25:59 -0300

    test load user by token in user mongo repository

--- src/modules/user/repositories/user-mongo-repository.spec.ts
+++ src/modules/user/repositories/user-mongo-repository.spec.ts
@@ -1,9 +1,12 @@
 import { UserMongoRepository } from './user-mongo-repository';
 import { MongoHelper } from '@/bin/helpers/db/mongo/mongo-helper';
-import { Collection } from 'mongodb';
+import { Collection, ObjectId } from 'mongodb';
 import { mockFakeUserData } from '@/modules/user/models/mocks/mock-user';
 import { MongoRepository } from '@/bin/repository/mongo-repository';
 import MockDate from 'mockdate';
+import variables from '@/bin/configuration/variables';
+import jwt from 'jsonwebtoken';
+
 let userCollection: Collection;
 
 describe('User Mongo Repository', () => {
@@ -56,6 +59,32 @@ describe('User Mongo Repository', () => {
   test('Should return null user if loadByEmail fails', async () => {
     const sut = makeSut();
     const user = await sut.loadByEmail('any_email@mail.com');
+    expect(user).toBeFalsy();
+  });
+  test('Should return an user loadByToken success with role', async () => {
+    const sut = makeSut();
+    const userAdd = {
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+      role: 'client',
+    };
+    const { ops } = await userCollection.insertOne(userAdd);
+    const { _id } = ops[0];
+    const token = await jwt.sign({ _id }, variables.Security.secretKey);
+    const user = await sut.loadByToken(token, 'client');
+    expect(user).toBeTruthy();
+    expect(user._id).toBeTruthy();
+    expect(user.name).toBe('any_name');
+    expect(user.email).toBe('any_email@mail.com');
+  });
+  test('Should return null user if loadByToken fails', async () => {
+    const sut = makeSut();
+    const token = await jwt.sign(
+      { _id: new ObjectId() },
+      variables.Security.secretKey,
+    );
+    const user = await sut.loadByToken(token, 'client');
     expect(user).toBeFalsy();
   });
 });
