45de3a4f536f8bf36c8e416bb6fba3e38cfd8767
Gustavo <imartytk@hotmail.com>
2020-09-16 13:18:45 -0300

    repository for use-case load-user-by-token

--- src/modules/user/repositories/user-mongo-repository.ts
+++ src/modules/user/repositories/user-mongo-repository.ts
@@ -4,9 +4,19 @@ import { AddUserModel } from '@/modules/user/usecases/add-user/add-user';
 import { UserModel } from '@/modules/user/models/user-model';
 import { MongoRepository } from '@/bin/repository/mongo-repository';
 import { LoadUserByEmailRepository } from './protocols/load-user-by-email-repository';
+import variables from '@/bin/configuration/variables';
+import jwt from 'jsonwebtoken';
+import { ObjectId } from 'mongodb';
+import { LoadUserByTokenRepository } from './protocols/load-user-by-token-repository';
 export class UserMongoRepository
-  implements AddUserRepository, LoadUserByEmailRepository {
+  implements
+    AddUserRepository,
+    LoadUserByEmailRepository,
+    LoadUserByTokenRepository {
   constructor(private readonly mongoRepository: MongoRepository) {}
+  userModel: UserModel;
+  role: string;
+  token: string;
 
   async add(userData: AddUserModel): Promise<UserModel> {
     const result = await this.mongoRepository.add(userData);
@@ -16,5 +26,15 @@ export class UserMongoRepository
   async loadByEmail(email: string): Promise<UserModel> {
     const result = await this.mongoRepository.getOne({ email });
     return result;
+  }
+  async loadByToken(token: string, role: string): Promise<UserModel> {
+    const decoded: any = await jwt.verify(token, variables.Security.secretKey);
+    const { _id } = decoded;
+    let query: any = { _id: new ObjectId(_id) };
+    if (role) {
+      query.role = role;
+    }
+    const result = await this.mongoRepository.getOne(query);
+    return result && MongoHelper.mapPassword(result);
   }
 }
